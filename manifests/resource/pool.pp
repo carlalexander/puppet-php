6a74f25d79ab63e65d52eb18e129c4c9885f3174
Carl Alexander <carlalexander@gmail.com>
2013-02-06 16:40:35 -0500

    Syntax error

--- manifests/resource/pool.pp
+++ manifests/resource/pool.pp
@@ -27,21 +27,21 @@
 #
 #  php::resource::pool { 'www': }
 define php::resource::pool
-  $user                      = 'www-data'
-  $group                     = 'www-data'
-  $listen                    = '/var/run/php5-fpm.sock'
-  $listen_owner              = 'www-data'
-  $listen_group              = 'www-data'
-  $listen_mode               = '0666'
-  $listen_allowed_clients    = '127.0.0.1'
-  $pm                        = 'dynamic'
-  $pm_max_children           = 5
-  $pm_start_servers          = 2
-  $pm_min_spare_servers      = 1
-  $pm_max_spare_servers      = 3
-  $pm_process_idle_timeout   = '10s'
-  $request_terminate_timeout = 30
-  $security_limit_extensions = '.php'
+  $user                      = 'www-data',
+  $group                     = 'www-data',
+  $listen                    = '/var/run/php5-fpm.sock',
+  $listen_owner              = 'www-data',
+  $listen_group              = 'www-data',
+  $listen_mode               = '0666',
+  $listen_allowed_clients    = '127.0.0.1',
+  $pm                        = 'dynamic',
+  $pm_max_children           = 5,
+  $pm_start_servers          = 2,
+  $pm_min_spare_servers      = 1,
+  $pm_max_spare_servers      = 3,
+  $pm_process_idle_timeout   = '10s',
+  $request_terminate_timeout = 30,
+  $security_limit_extensions = '.php',
 ) {
   File {
     owner => 'root',
